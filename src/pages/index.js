60a85a0a4bc87e10374209046620be35aae7edea
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-05 14:36:20 -0400

    changed caps

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Nav from '@/components/nav'
-import Hero from '@/components/Hero'
+import Hero from '@/components/hero'
 
 
 const inter = Inter({ subsets: ['latin'] })
