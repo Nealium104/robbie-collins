be923ebf40d7163c4546757b1e170e4cd0c5d28c
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-05 14:46:32 -0400

    just fixed the nav so there's a home button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ export default function Nav () {
     return (
         <div className="navbar bg-base-100">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">Robbie Collins</a>
+    <Link href="/" className="btn btn-ghost normal-case text-xl"> Robbie Collins</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
