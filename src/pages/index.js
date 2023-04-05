3508d02349860a8d77bb45bbaebc9d037dd2155a
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-05 14:35:05 -0400

    changed import caps

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Nav from '@/components/Nav'
+import Nav from '@/components/nav'
 import Hero from '@/components/Hero'
 
 
