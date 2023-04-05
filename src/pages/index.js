ac5952d64940a24c06fb2dae6acfcc7aa1357d1c
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-05 14:40:07 -0400

    src

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Nav from '@/components/nav'
-import Hero from '@/components/hero'
+import Nav from '@/src/components/Nav'
+import Hero from '@/src/components/Hero'
 
 
 const inter = Inter({ subsets: ['latin'] })
--- src/pages/teaching.js
+++ src/pages/teaching.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Nav from "@/components/nav";
+import Nav from '@/src/components/Nav'
 
 
 export default function Teaching () {
