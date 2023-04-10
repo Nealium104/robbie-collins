79c28120aa10d829cb2273ebbcb0f85d3f240101
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-09 22:50:56 -0400

    added a footer with social media links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Nav from '../components/Nav'
 import ContactForm from "../components/ContactForm";
+import Footer from "@/components/Footer";
 
 export default function Contact () {
     return (
         <>
             <Nav />
             <ContactForm />
+            <Footer />
         </>
     )
 }
