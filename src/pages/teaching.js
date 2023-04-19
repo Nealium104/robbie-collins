3f93cc970c9fa6f08542fac2dd38fed64b103820
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-19 12:43:18 -0400

    added shadows and Links to the contact page

--- src/pages/teaching.js
+++ src/pages/teaching.js
@@ -1,6 +1,7 @@
 import Nav from '../components/Nav'
 import Footer from "../components/Footer"
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Teaching () {
     return (
@@ -22,7 +23,7 @@ export default function Teaching () {
                     </div>
                 </div>
                 <div className="flex flex-col md:flex-row items-center md:h-40 my-10">
-                    <div className="flex flex-col justify-around text-center p-4 m-4 bg-white text-black rounded-xl w-3/4 h-full gap-6 hover:bg-primary hover:text-white">
+                    <Link href="/contact/" className="flex flex-col justify-around text-center p-4 m-4 bg-white text-black rounded-xl w-3/4 h-full gap-6 hover:bg-primary hover:text-white shadow-xl shadow-black-900">
                         <h2 className="underline text-3xl font-bold">Individual</h2>
                         <div className="h-1/2">
                             <div>
@@ -32,15 +33,15 @@ export default function Teaching () {
                                 <span className='text-xl'>1 Hour | $50</span>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex flex-col justify-evenly text-center p-4 m-4 bg-white text-black rounded-xl w-3/4 h-full gap-6 hover:bg-primary hover:text-white">
+                    </Link>
+                    <Link href="/contact/" className="flex flex-col justify-evenly text-center p-4 m-4 bg-white text-black rounded-xl w-3/4 h-full gap-6 hover:bg-primary hover:text-white shadow-xl shadow-black-900">
                         <h2 className="underline text-3xl font-bold">Monthly</h2>
                         <span className='text-xl h-1/2'>1 Hour | $180</span>
-                    </div>
-                    <div className="flex flex-col justify-between text-center p-4 m-4 bg-white text-black rounded-xl w-3/4 h-full gap-6 hover:bg-primary hover:text-white">
+                    </Link>
+                    <Link href="/contact/" className="flex flex-col justify-between text-center p-4 m-4 bg-white text-black rounded-xl w-3/4 h-full gap-6 hover:bg-primary hover:text-white shadow-xl shadow-black-900">
                         <h2 className="underline text-3xl font-bold">Yearly</h2>
                         <span className='text-xl h-1/2'>1 Hour | $2,000</span>
-                    </div>
+                    </Link>
                 </div>
             </div>
             <Footer />
