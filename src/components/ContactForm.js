cd87886988e3c4f670ecfcad1ee5eccf6bbe9373
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-19 13:44:36 -0400

    finalized layout for teaching

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,62 +17,40 @@ export default function ContactForm () {
 
     return (
     <>
-    <div className="flex justify-center py-3">
-            <div className="card w-96 bg-primary shadow-2xl items-center">
-            <div>
-                <figure className="px-10 pt-10">
-                    <Image 
-                    src="/contact.webp" 
-                    alt="Shoes" 
-                    className="rounded-xl"
-                    width = {2000}
-                    height = {3000}
-                    priority
-                    />
-                </figure>
+        <div className="flex justify-center py-3">
+            <div className="card w-96 bg-primary/50 shadow-2xl items-center">
+                <div>
+                    <figure className="px-10 pt-10">
+                        <Image 
+                        src="/contact.webp" 
+                        alt="Shoes" 
+                        className="rounded-xl"
+                        width = {400}
+                        height = {600}
+                        priority
+                        />
+                    </figure>
+                </div>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Contact me for opportunities and lessons here:</h2>
+                        <form action="post" onSubmit={handleOnSubmit}>
+                            <div>
+                                <label htmlFor="name">Name:</label>
+                                <input type="text" name="name" placeholder="Type your name here" className="input input-bordered w-full max-w-xs" />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" name="email" placeholder="Type your email here" className="input input-bordered w-full max-w-xs" />
+                            </div>
+                            <div>
+                                <label htmlFor="message">Message:</label>
+                                <textarea name="message" className="textarea textarea-bordered block w-full" placeholder="Type your message here"></textarea>
+                            </div>
+                            <button className="btn btn-neutral my-4">Submit</button>
+                        </form>
+                </div>
             </div>
-        <div className="card-body items-center text-center">
-            <h2 className="card-title">Contact me here:</h2>
-                <form action="post" onSubmit={handleOnSubmit}>
-                    <div>
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" name="name" placeholder="Type your name here" className="input input-bordered w-full max-w-xs" />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" placeholder="Type your email here" className="input input-bordered w-full max-w-xs" />
-                    </div>
-                    <div>
-                        <label htmlFor="message">Message:</label>
-                        <textarea name="message" className="textarea textarea-bordered block w-full" placeholder="Type your message here"></textarea>
-                    </div>
-                    <button className="btn btn-neutral my-4">Submit</button>
-                </form>
         </div>
-        </div>
-    </div>
     </>
     )
 }
-
-// export default function ContactForm () {
-//     return (
-//         <div className="flex justify-center items-center">
-//             <div className="flex flex-col justify-center items-center border-2 border-primary rounded-md w-1/4 bg-base-300 p-10">
-//                 <h1 className="font-bold">Contact me here:</h1>
-//                 <div>
-//                     <span>Name:</span>
-//                     <input type="text" placeholder="Type your name here" className="input input-bordered w-full max-w-xs" />
-//                 </div>
-//                 <div>
-//                     <span>Email:</span>
-//                     <input type="email" placeholder="Type your email here" className="input input-bordered w-full max-w-xs" />
-//                 </div>
-//                 <div>
-//                     <span>Message:</span>
-//                     <textarea className="textarea textarea-bordered block" placeholder="Type your message here"></textarea>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -39,8 +39,8 @@ export default function Nav () {
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
             </a>
             <ul className="p-2 bg-base-100/75 z-10">
-              <li><Link href="/recordings/" className='hover:text-primary'>Recordings</Link></li>
-              <li><Link href="/teaching/" className='hover:text-primary'>Teaching</Link></li>
+              <li><Link href="/recordings/" className='hover:text-primary py-2'>Recordings</Link></li>
+              <li><Link href="/teaching/" className='hover:text-primary py-2'>Teaching</Link></li>
             </ul>
           </li>
           <li><Link href="/contact/" className='hover:text-primary'>Contact</Link></li>
