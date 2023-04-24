508f3f8ee8a2385ba8c6e2371adeb2888e6cdf75
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-24 18:54:15 -0400

    tried a different color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,38 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: ["business",],
+    themes: [{
+      mytheme: {
+        "primary-100":"#F7D488",
+        "primary":"#d7b66c",
+        "primary-300":"#90742d",
+        "accent-100":"#F7A541",
+        "accent-200":"#8a4a00",
+        "text-100":"#FFFFFF",
+        "text-200":"#e0e0e0",
+        "bg-100":"#0F1626",
+        "base-100":"#1e2436",
+        "base-200":"#363c4e",   
+      }
+    },
+  ],
+  },
     base: false,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
     darkTheme: "business",
-  },
-}
+  }
+
+// --primary-100:#F7D488;
+// --primary-200:#d7b66c;
+// --primary-300:#90742d;
+// --accent-100:#F7A541;
+// --accent-200:#8a4a00;
+// --text-100:#FFFFFF;
+// --text-200:#e0e0e0;
+// --bg-100:#0F1626;
+// --bg-200:#1e2436;
+// --bg-300:#363c4e;
+  
