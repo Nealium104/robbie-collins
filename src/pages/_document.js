e18d4ddf7af927ee0a86d6de5bc14944db731695
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-28 14:43:24 -0400

    added google verificatin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <meta
         http-equiv="Content-Security-Policy"
-        content="default-src 'self' data:; script-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://vitals.vercel-insights.com/; style-src 'self' 'unsafe-inline'; frame-src 'self' https://www.google.com/recaptcha/; img-src 'self' data:;"
+        content="default-src 'self' data:; script-src 'self' https://www.google.com/recaptcha/api/siteverify https://www.gstatic.com/recaptcha/ https://vitals.vercel-insights.com/; style-src 'self' 'unsafe-inline'; frame-src 'self' https://www.google.com/recaptcha/; img-src 'self' data:;"
       />
       </Head>
       <body>
