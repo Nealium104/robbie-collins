dba06389457e5f91bc79b387579f998546175b09
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-28 13:55:31 -0400

    modified the doc file to allow captcha

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const csp = `
+      default-src 'self';
+      script-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ 'unsafe-inline';
+      frame-src 'self' https://www.google.com/recaptcha/;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data:;
+    `;
+    return { ...initialProps, csp };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta httpEquiv="Content-Security-Policy" content={this.props.csp} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
