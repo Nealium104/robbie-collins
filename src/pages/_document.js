99003a6436657df47ac064b0a9ff49fb9637eab7
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-30 09:22:03 -0400

    changed CSP

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta
-        http-equiv="Content-Security-Policy"
-        content="default-src 'self' data:; script-src 'self' https://www.google.com/recaptcha/api/siteverify https://www.gstatic.com/recaptcha/ https://vitals.vercel-insights.com/; style-src 'self' 'unsafe-inline'; frame-src 'self' https://www.google.com/recaptcha/; img-src 'self' data:;"
-      />
       </Head>
       <body>
         <Main />
