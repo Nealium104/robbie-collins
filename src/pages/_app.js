607c58d7373b565f9ad4476e5852afcca6722af6
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-30 23:27:10 -0400

    changed website description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }) {
       `}</style>
       <Head>
         <title>Robbie Collins</title>
-        <meta name="description" content="Robbie Collins" />
+        <meta name="description" content="Robbie Collins's professional website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
