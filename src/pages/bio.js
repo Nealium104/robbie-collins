fa56992a58f9b01e633f60bf745f686b618fbdf6
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-05-26 09:22:53 -0400

    Added bio lines and swapped a photo

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -65,7 +65,7 @@ export default function Bio () {
                             />                    
                         </animated.div>
                             <p className="font-thin md:w-3/4 text-xl">
-                            Robert has performed in groups such as the Cincinnati Symphony Orchestra, May Festival Herald Trumpet Ensemble, Knoxville Symphony Orchestra, the Lexington Brass Band, Saxton's Cornet Band, and the ECU Faculty Brass Quintet. He has also performed at events including the International Trumpet Guild New Works Recital, North American Baroque Trumpet Conference, the National Trumpet Competition, Kentucky Music Educators Conference, Midwest Regional Tuba Euphonium Conference, as well as others. One of Robert’s strengths is his ability to cover a wide ranging set of diverse styles; however, he feels most at home in a chamber music setting.
+                            Robert has performed in groups such as the Cincinnati Symphony Orchestra May Festival Herald Trumpet Ensemble, Knoxville Symphony Orchestra, the Lexington Brass Band, Saxton's Cornet Band, the ECU Faculty Brass Quintet, and UK faculty brass quintet. He has also performed at events including the International Trumpet Guild New Works Recital, North American Baroque Trumpet Conference, the National Trumpet Competition, Kentucky Music Educators Conference, Midwest Regional Tuba Euphonium Conference, as well as others. One of Robert’s strengths is his ability to cover a wide ranging set of diverse styles; however, he feels most at home in a chamber music setting.
                             </p>
                     </div>
                     <div className="bg-black/25 p-10 md:flex md:items-center">
@@ -84,26 +84,26 @@ export default function Bio () {
                                 />
                         </animated.div>
                         <p className="font-thin p-4 md:w-3/4 text-xl">
-                        Currently, Robert serves as the co-chair for the International Trumpet Guild (ITG) Pre-Professional Division and acts as a ITG Youth Competition Coach. He also serves as the primary volunteer coordinator for the Historic Brass Society. He has previously worked with the University of Kentucky Summer Trumpet Institute, International Trumpet Guild Conference, North American Baroque Trumpet Competition, and Gardner-Webb Trumpet Summit. He has also been an attendee at the Chosen Vale International Trumpet Seminar during his time researching his dissertation.
+                        Currently, Robert serves as the co-chair for the International Trumpet Guild (ITG) Pre-Professional Division. He also serves as the primary volunteer coordinator for the Historic Brass Society. He has previously worked with the University of Kentucky Summer Trumpet Institute, International Trumpet Guild Conference, North American Baroque Trumpet Competition, and Gardner-Webb Trumpet Summit. He has also been an attendee at the Chosen Vale International Trumpet Seminar during his time researching his dissertation.
                         </p>
                     </div>
                     <div className="bg-black/75 p-10 md:flex md:flex-row-reverse md:items-center">
                         <animated.div className="md:w-1/2" style={createSpring(4)}> 
                             <Image 
-                                src="/images/baroqueDuet.jpg" 
-                                alt="baroque duet"
+                                src="/images/heraldTrumpets.jpg" 
+                                alt="Herald Trumpet Ensemble"
                                 width={400}
                                 height={400}
                                 layout="responsive"
                                 objectFit="cover"
                                 placeholder='blur'
-                                blurDataURL='/blur/baroqueDuetBlur.jpg'
+                                blurDataURL='/blur/heraldTrumpetsBlur.jpg'
                                 onLoad={handleImageLoad(4)}
                                 spring={createSpring(4)}
                             />
                         </animated.div>
                         <p className="font-thin p-4 md:w-3/4 text-xl">
-                        In addition to running a private trumpet studio, Robert is a teaching assistant at the University of Kentucky, teaching the course MUC163 (Brass Methods), and instructing undergraduate trumpet students in lessons. Prior to this, Robert served as a graduate assistant at East Carolina University. His primary duty for this was performing with the faculty brass quintet in several master classes in performances across North Carolina at various high schools and middle schools. Along with this, he also served as a teaching assistant for Jazz History, and served as an assistant audio engineer for East Carolina University.
+                        In addition to running a private trumpet studio, Robert is a teaching assistant at the University of Kentucky, an ITG Youth Competition Coach, teaching the course MUC163 (Brass Methods), and instructing undergraduate trumpet students in lessons. Prior to this, Robert served as a graduate assistant at East Carolina University. His primary duty for this was performing with the faculty brass quintet in several master classes in performances across North Carolina at various high schools and middle schools. Along with this, he also served as a teaching assistant for Jazz History, and served as an assistant audio engineer for East Carolina University.
                         </p>
                     </div>
                     <div className="bg-black/25 p-10 md:flex md:items-center">
@@ -150,3 +150,15 @@ export default function Bio () {
         </div>
     )
 }
+
+{/* <div className="bg-black/50 md:flex md:flex-row-reverse md:items-center">
+<p className="p-4 md:w-1/2 text-xl">
+
+</p>
+<div className="md:w-1/2">
+    <Image 
+
+    />
+</div>
+</div> */}
+
